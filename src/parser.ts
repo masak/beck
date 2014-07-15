145653d525f5df987725ff049d2f043f3e034838
Carl Masak <cmasak@gmail.com>
2014-07-15 12:06:39 +0200

    parse some simple html

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,64 @@
 export = Parser;
 
 class Parser {
-    parse(input) { return [] }
+    parse(input : string) {
+        var result = [];
+
+        var pos = 0;
+        function eof() {
+            return pos >= input.length;
+        }
+
+        function make(type : string, cb : (node) => void) {
+            var node = { type: type, start: pos, end: 0, zygs: [] };
+            cb(node);
+            node.end = pos;
+
+            return node;
+        }
+
+        function expect(s : string) {
+            // TODO: error handling
+            pos += s.length;
+        }
+
+        function identifier() {
+            return make('identifier', node => {
+                pos += input.substring(pos).match(/\w+/)[0].length;
+            });
+        }
+
+        function start_tag() {
+            return make('start-tag', node => {
+                expect('<');
+                node.zygs.push(identifier());
+                expect('>');
+            });
+        }
+        function content() {
+            // TODO: handle content
+            return make('content', node => {});
+        }
+        function end_tag() {
+            return make('end-tag', node => {
+                expect('</');
+                node.zygs.push(identifier());
+                expect('>');
+            });
+        }
+
+        function element() {
+            return make('element', node => {
+                node.zygs.push(start_tag());
+                node.zygs.push(content());
+                node.zygs.push(end_tag());
+            });
+        }
+
+        while (!eof()) {
+            result.push(element());
+        }
+
+        return result;
+    }
 }
--- test/parser.ts
+++ test/parser.ts
@@ -10,4 +10,50 @@ describe('HTML parser', function(){
         var result = p.parse('');
         assert.deepEqual(result, []);
     });
+
+    it('should return simple syntax tree on correct HTML', () => {
+        var p = new parser;
+        var result = p.parse('<html></html>');
+        assert.deepEqual(result, [
+            {
+                type: 'element',
+                start: 0,
+                end: 13,
+                zygs: [
+                    {
+                        type: 'start-tag',
+                        start: 0,
+                        end: 6,
+                        zygs: [
+                            {
+                                type: 'identifier',
+                                start: 1,
+                                end: 5,
+                                zygs: []
+                            }
+                        ]
+                    },
+                    {
+                        type: 'content',
+                        start: 6,
+                        end: 6,
+                        zygs: []
+                    },
+                    {
+                        type: 'end-tag',
+                        start: 6,
+                        end: 13,
+                        zygs: [
+                            {
+                                type: 'identifier',
+                                start: 8,
+                                end: 12,
+                                zygs: []
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]);
+    });
 });
